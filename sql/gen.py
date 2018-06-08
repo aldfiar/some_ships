047299a366702d1f21082736ffd0a168d653fd95
ivan.schekoldin <iv.sheckoldin@yandex.ru>
2018-06-08 23:37:58 +0300

    add create command create tables

--- sql/gen.py
+++ sql/gen.py
@@ -1,6 +1,49 @@
 import sqlite3
 
-def conn(name):
-    return qlite3.connect(name)
+tables = {"engines.db": "CREATE TABLE IF NOT EXISTS ENGINES("
+                        "engine text PRIMARY KEY,"
+                        "power int,"
+                        "type int"
+                        ")",
+          "hulls.db": "CREATE TABLE IF NOT EXISTS HULLS"
+                      "(hull text PRIMARY KEY,"
+                      "armor int,"
+                      "type int,"
+                      "capacity int"
+                      ")",
+          "weapons.db": "CREATE TABLE IF NOT EXISTS WEAPONS("
+                        "weapon text PRIMARY KEY,"
+                        "reload_speed int,"
+                        "rotation_speed int,"
+                        "diameter int,"
+                        " power_volley int,"
+                        " count int"
+                        ")",
+          "ships.db": "CREATE TABLE IF NOT EXISTS SHIPS"
+                      "(ship text PRIMARY KEY,"
+                      "weapon text,"
+                      "hull text,"
+                      "engine text,"
+                      "FOREIGN KEY (weapon) REFERENCES weapons(weapon),"
+                      "FOREIGN KEY (engine) REFERENCES engines(engine),"
+                      "FOREIGN KEY (hull) REFERENCES hulls(hull)"
+                      ")"
+          }
 
-def create_engines()
+
+def create_table(name, model):
+    try:
+        conn = sqlite3.connect(name)
+        cur = conn.cursor()
+        cur.execute(model)
+    except Exception:
+        raise Exception("Try to execute %s" % model)
+
+
+def insert(name, value):
+    pass
+
+
+if __name__ == "__main__":
+    for table in tables:
+        create_table(table, tables.get(table))
