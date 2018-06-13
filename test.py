0a0beae6fab23185aaf7116a0d030adb10b0225a
ivan.schekoldin <iv.sheckoldin@yandex.ru>
2018-06-13 08:56:26 +0300

    add sql file

--- test.py
+++ test.py
@@ -1,51 +1,57 @@
-import unittest
 from shutil import copyfile
 
+import nose
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from generator import Randomizer
-from objects.parts import Base
+from objects.parts import Base, Weapon, Ship, Engine, Hull
+
+db_file = "ships.sqlite"
+backup_file = "ships_before.sqlite"
 
 
-class TestShips(unittest.TestCase):
-
-    def setUp(self):
-        db_file = "ships.sqlite"
-        backup_file = "ships_before.sqlite"
-        self.engine = create_engine('sqlite:///{}'.format(db_file))
-        Base.metadata.create_all(self.engine)
-        randomizer = Randomizer(self.engine)
+class TestShips(object):
+    @classmethod
+    def setupClass(cls):
+        cls.engine = create_engine('sqlite:///{}'.format(db_file))
+        Base.metadata.create_all(cls.engine)
+        randomizer = Randomizer(cls.engine)
         randomizer.initialize()
         copyfile(db_file, backup_file)
         randomizer.randomize()
-        self.unchanged_engine = create_engine('sqlite:///{}'.format(backup_file))
-        self.Session = sessionmaker(bind=self.engine, expire_on_commit=False)
-        self.Session_unchanged = sessionmaker(bind=self.unchanged_engine, expire_on_commit=False)
+        cls.unchanged_engine = create_engine('sqlite:///{}'.format(backup_file))
+        cls.session = sessionmaker(bind=cls.engine, expire_on_commit=False)
+        cls.session_unchanged = sessionmaker(bind=cls.unchanged_engine, expire_on_commit=False)
 
-    def tearDown(self):
-        Base.metadata.drop_all(self.engine)
+    @classmethod
+    def tearDownClass(cls):
+        Base.metadata.drop_all(cls.engine)
 
     def test_weapons(self):
-        pass
+        for new in self.session().query(Ship):
+            old = self.session_unchanged().query(Ship).get(new.ship)
+            yield self.check_ship, new, old, Weapon, "weapon"
 
     def test_engines(self):
-        pass
+        for new in self.session().query(Ship):
+            old = self.session_unchanged().query(Ship).get(new.ship)
+            yield self.check_ship, new, old, Engine, "engine"
 
     def test_hulls(self):
-        pass
+        for new in self.session().query(Ship):
+            old = self.session_unchanged().query(Ship).get(new.ship)
+            yield self.check_ship, new, old, Hull, "hull"
 
-    def test_ships(self):
-        pass
+    def check_ship(self, new, old, parameter, key):
+        self.check(Ship, new, old)
+        new_param = self.session().query(parameter).get(getattr(new, key))
+        old_param = self.session_unchanged().query(parameter).get(getattr(new, key))
+        self.check(parameter, new_param, old_param)
 
-    def check_weapon(self, session):
-        pass
-
-    def check_engine(self, engine):
-        pass
-
-    def check_hull(self, hull):
-        pass
-
-    def check_ship(self, ship):
-        pass
+    def check(self, table, new, old):
+        for element in table.variables():
+            new_element = getattr(new, element)
+            old_element = getattr(old, element)
+            nose.tools.eq_(new_element, old_element,
+                           msg="Expected {type}: {old}, was {new}".format(type=table, old=old_element, new=new_element))
